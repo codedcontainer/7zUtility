b0cc90a41f6741a4394eda13d0d733a23b58ac3e
Jason <jdmagic21@gmail.com>
2021-03-25 10:26:06 -0400

    init and prompts

---
+++ main.py
@@ -0,0 +1,45 @@
+#%% 
+import os
+import getpass
+
+#%%
+project_description = "Compress each top level directory(s) and/or file(s) listed at a specific path location into seperate compressed files without needing to use 7zip commands for each individual item. You will only need to provide one password for a collection of items."
+print(project_description)
+
+# %%
+system_process = os.system('7z')
+if(system_process == 32512):
+    print('7z cli tools not installed')
+    input('Press enter to end program.')
+else:
+    compress = ""
+    while(compress == ""):
+        compress = str.lower(input('Compress or uncompress all (compress/uncompress): '))
+        if(compress == "compress" or compress == "uncompress"):
+           break
+        else:
+            print('Input must be either "compress" or "uncompress"')
+            compress = ""
+# %%
+
+    dir_loc = ""
+    while(dir_loc == ""):
+        dir_loc = input('Input directory for files/folders compression/uncompression:')
+        if(dir_loc == ""):
+             print('No input detected. Directory cannot be found.')
+        else:
+            files = []
+            try:
+                files = os.listdir(dir_loc)
+                if(len(files) == 0):
+                    print('Directory does not contain any files or folders')
+                    dir_loc = ""
+                else:
+                    break
+            except FileNotFoundError:
+                dir_loc = ""
+                print('Directory does not exist')
+
+            
+
+# %%
