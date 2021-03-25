d793a49f5bef6feca8014af02d76fe1621538563
Jason <jdmagic21@gmail.com>
2021-03-25 11:17:53 -0400

    password verification

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #%% 
 import os
 import getpass
+import re
 
 #%%
 project_description = "Compress each top level directory(s) and/or file(s) listed at a specific path location into seperate compressed files without needing to use 7zip commands for each individual item. You will only need to provide one password for a collection of items."
@@ -44,13 +45,30 @@ else:
 
 # %%
     password_protect = ""
+    password = "" 
     while(password_protect == ""):
         password_protect = str.lower(input('password protect all files and folders (y/n)? '))
-        if(password_protect == "y" or password_protect == "n"):
-            break
+        if(password_protect == "y" or password_protect == "n"): 
+            if(password_protect == "y"):
+                while(password == ""):
+                    password = getpass.getpass("Password: ")
+                    if(len(password) < 10):
+                        print('Length of password must be greater than 10 characters')        
+                    if(re.search(r"\d", password ) == None):
+                        print('Password must contain at least one digit')
+                    if(re.search(r"[!@#$%^&*:<?]", password) == None):
+                        print('Password must contain at least one of the following special characters !@#$%^&*:<?')
+
+                    password_verify = getpass.getpass("Verify Password: ")
+                    if(password != password_verify):
+                        print("Password does not match, try again.")
+                        password = ""
+                    else:
+                        break
+            else:
+                break
         else:
             print('Input must be either "y" or "n"')
             password_protect = ""
-
 
 # %%
