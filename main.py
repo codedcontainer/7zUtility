59350c4a6a570d82da016bce79c97fa8bbd66364
Jason <jdmagic21@gmail.com>
2021-03-25 13:39:25 -0400

    refactoring

--- main.py
+++ main.py
@@ -1,81 +1,88 @@
-#%% 
 import os
 import subprocess
 import getpass
 import re
 
-#%%
-project_description = "Compress each top level directory(s) and/or file(s) listed at a specific path location into seperate compressed files without needing to use 7zip commands for each individual item. You will only need to provide one password for a collection of items."
+project_description = """
+*****************************************************************
+|          Python 7z File Compression Utility Program           |
+*****************************************************************
+| Compress each top level directory(s) and/or file(s)           |
+| listed at a specific path location into seperate compressed   | 
+| files without needing to use 7zip commands for each           | 
+| individual item. You will only need to provide one            |
+| password for a collection of items.                           |
+*****************************************************************
+"""
 print(project_description)
 
-# %%
-system_process = os.system('7z')
-if(system_process == 32512):
+try:
+    subprocess.check_output('7z')
+except OSError:
     print('7z cli tools not installed')
-    input('Press enter to end program.')
-else:
-    compress = ""
-    while(compress == ""):
-        compress = str.lower(input('Compress or uncompress all (compress/uncompress): '))
-        if(compress == "compress" or compress == "uncompress"):
-           break
-        else:
-            print('Input must be either "compress" or "uncompress"')
-            compress = ""
-# %%
+    input('Press enter to end program: ')
+    exit()
 
-    dir_loc = ""
-    files = []
-    while(dir_loc == ""):
-        dir_loc = input('Input directory for files/folders compression/uncompression:')
-        if(dir_loc == ""):
-             print('No input detected. Directory cannot be found.')
-        else:
-            try:
-                files = os.listdir(dir_loc)
-                if(len(files) == 0):
-                    print('Directory does not contain any files or folders')
-                    dir_loc = ""
-                else:
-                    break
-            except FileNotFoundError:
+compress = ""
+while(compress == ""):
+    compress = str.lower(input('Compress or uncompress all (compress/uncompress): '))
+    if(compress == "compress" or compress == "uncompress"):
+        break
+    else:
+        print('Input must be either "compress" or "uncompress"')
+        compress = ""
+
+dir_loc = ""
+files = []
+while(dir_loc == ""):
+    dir_loc = input('Parent target directory of files/folders:')
+    if(dir_loc == ""):
+            print('Target directory cannot be found.')
+    else:
+        try:
+            files = os.listdir(dir_loc)
+            if(len(files) == 0):
+                print('Target directory does not contain any files or folders')
                 dir_loc = ""
-                print('Directory does not exist')
+            else:
+                break
+        except FileNotFoundError:
+            dir_loc = ""
+            print('Taget directory does not exist')
 
-# %%
-    password_protect = ""
-    password = "" 
-    while(password_protect == ""):
-        password_protect = str.lower(input('password protect all files and folders (y/n)? '))
-        if(password_protect == "y" or password_protect == "n"): 
-            if(password_protect == "y"):
-                while(password == ""):
-                    password = getpass.getpass("Password: ")
+password_protect = ""
+password = "" 
+while(password_protect == ""):
+    password_protect = str.lower(input('Password protection for all files and folders (y/n)? '))
+    if(password_protect == "y" or password_protect == "n"): 
+        if(password_protect == "y"):           
+            while(password == ""):
+                if(compress == "compress"):
+                    print("Password must be greate than 10 characters, contain one digit, and have at least one of the following characters: !@#$%^&*:<?")
+                
+                password = getpass.getpass("Password: ")
+                if(compress == "compress"):                    
                     if(len(password) < 10):
-                        print('Length of password must be greater than 10 characters')        
+                        print('Length of password must be greater than 10 characters')
+                        password = ""        
                     if(re.search(r"\d", password ) == None):
                         print('Password must contain at least one digit')
+                        password = ""
                     if(re.search(r"[!@#$%^&*:<?]", password) == None):
                         print('Password must contain at least one of the following special characters !@#$%^&*:<?')
+                        password = ""                
 
+                if(password != ""):                                  
                     password_verify = getpass.getpass("Verify Password: ")
                     if(password != password_verify):
                         print("Password does not match, try again.")
                         password = ""
-                    else:
-                        break
-            else:
-                break
         else:
-            print('Input must be either "y" or "n"')
-            password_protect = ""
+            break
+    else:
+        print('Input must be either "y" or "n"')
+        password_protect = ""
 
-#%%
-print(compress)
-
-# %%
-# if compressing
-print(password)
 for file in files:
     if(compress == "compress"):
         file = file.replace(" ", "-")
@@ -94,9 +101,6 @@ for file in files:
     else:
         if ".7z" in file:
             if(password != ""):
-                os.system(f"cd {dir_loc} && 7z x -p'{password}' '{file}'")
+                os.system(f"cd {dir_loc} && 7z x -y -p'{password}' '{file}'")
             else:
-                os.system(f"cd {dir_loc} && 7z x '{file}'")
-
-
-# %%
+                os.system(f"cd {dir_loc} && 7z x -y '{file}'")
